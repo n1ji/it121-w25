468e83e9cc125de4eed1b8afd5972157f617233c
Myra Rudy <0.maysin.0@gmail.com>
2025-02-10 15:20:01 -0800

    Add feb5.js with utility functions for sum, max, and greeting

---
+++ feb5.js
@@ -0,0 +1,8 @@
+sumFunc = (wow, wowow) => wow + wowow;
+console.log(sumFunc(Math.PI, Math.E));
+
+maxNum = (numberNum, num2) => numberNum > num2 ? numberNum : num2;
+console.log(maxNum(3233, 5344));
+
+const greeting = (name) => `Hello, ${name}!`;
+console.log(greeting("John"));
